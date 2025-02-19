87380e103048fb2f7d9ecbddc22e24e93f6e7dda
gregatccv <59838316+gregatccv@users.noreply.github.com>
2025-02-19 15:24:28 -0500

    read_csv_array update

--- Start/Ch1/read_csv_array.py
+++ Start/Ch1/read_csv_array.py
@@ -8,6 +8,11 @@ import csv
 def read_csv_to_array(filename):
   # define the array that will hold the data
   data = []
+  with open(filename, 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+      data.append(row)
+  return
 
 # Read the data into an array of arrays
 inventory_data = read_csv_to_array("Inventory.csv")
